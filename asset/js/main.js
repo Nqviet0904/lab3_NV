5b10c79d2b2a4c1f27fc80db32dc1b73c085a916
Nqviet0904 <111555381+Nqviet0904@users.noreply.github.com>
2022-11-01 11:15:07 +0700

    first commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,7 +1,42 @@
 var search = document.getElementById("demo");
-
-search.addEventListener("keypress",function(e){
-  if (e.keyCode ===13 && search.value.length>0){
-    window.location = "./timkiem.html"
+var h1elements = document.getElementsByTagName("h1");
+var pelements = document.getElementsByTagName("p");
+var h2elements = document.getElementsByTagName("h2");
+var text = "";
+search.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13 && search.value.length > 0) {
+    for (var i = 0; i < h1elements.length; i++) {
+      if (
+        h1elements[i].textContent
+          .toUpperCase()
+          .includes(search.value.toUpperCase())
+      ) {
+        var htmls = `<h1>${h1elements[i].textContent}</h1>`;
+        text = text + htmls;
+      }
+    }
+    for (var i = 0; i < h2elements.length; i++) {
+      console.log(
+        h2elements[i].innerText.toUpperCase().includes(search.value.toUpperCase())
+      );
+      if (
+        h2elements[i].innerText.toUpperCase().includes(search.value.toUpperCase())
+      ) {
+        var htmls = `<h2>${h2elements[i].innerText}</h2>`;
+        text = text + htmls;
+      }
+    }
+    for (var i = 0; i < pelements.length; i++) {
+        console.log(pelements[i].innerText.toUpperCase().includes(search.value.toUpperCase())
+      );
+      if (
+        pelements[i].innerText.toUpperCase().includes(search.value.toUpperCase())
+      ) {
+        var htmls = `<li>${pelements[i].innerText}</li>`;
+        text = text + htmls;
+      }
+    }
+    window.localStorage.setItem("search", text);
+    window.location.href = "./timkiem.html";
   }
-})
+});
---
+++ asset/js/timkiem.js
@@ -0,0 +1,21 @@
+var search = document.getElementById("demo");
+// var respone = fetch('./trangchu.html');
+// respone.then((response)=>{
+//   response.json();
+// })
+// .then((data)=>console.log(data))
+var data = localStorage.getItem('search');
+
+var result = document.getElementById('result');
+result.innerHTML = data;
+
+
+// var form = document.querySelector('form');
+
+search.addEventListener("keypress",function(e){
+  if (e.keyCode ===13 && search.value.length>0){
+   window.location = "./timkiem.html"
+
+  }
+});
+
